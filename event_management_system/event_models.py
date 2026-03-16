ffbc22f13d471d92582d08c8fbadc181670c01d5
Toluwani Edgal <edgalmiracle@gmail.com>
2026-03-17 00:34:25 +0100

    Made changes to the event model file

--- event_management_system/event_models.py
+++ event_management_system/event_models.py
@@ -4,14 +4,14 @@ from event_enums import UserRole
 
 @dataclass
 class Event:
-    event_id:str # an integer, string or a combination of both of them is expected
-    title: str # a sting is expected
-    description: str #a string is expected
-    date_time: datetime # both the date, and time are expected
-    max_capacity: int # an integer is ecpected
-    category: str # a sting is expected
-    current_participants: int # an integer is ecpected
-    status: str # a sting is expected
+    event_id:str 
+    title: str 
+    description: str 
+    date_time: datetime 
+    max_capacity: int 
+    category: str
+    current_participants: int 
+    status: str 
 
     @property
     def is_full(self) -> bool: #checks if the number of participant is more than the maxium capacity required, and returns the complementary boolean answer
@@ -27,11 +27,11 @@ class Event:
     
 @dataclass
 class User:
-     user_id = "user_id"
-     name = "name"
-     email = "email"
-     role = "role"
+    user_id = "user_id"
+    name = "name"
+    email = "email"
+    role = "role"
 
-     @property
-     def is_admin(self) -> bool:
-          return self.role == UserRole.ADMIN
+    @property
+    def is_admin(self) -> bool:
+        return self.role == UserRole.ADMIN
