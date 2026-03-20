207a7ca43dcd7e5cbcb84933ae8be6839c38eb9b
Toluwani Edgal <edgaltoluwani@gmail.com>
2026-03-20 02:22:20 +0100

    Added new functions to the main file

--- event_management_system/main.py
+++ event_management_system/main.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-from event_database import get_connection, init_database
+from event_database import init_database, get_connection
 import event_config
 from event_repository import EventRepository
 from user_repository import UserRepository
@@ -17,5 +17,37 @@ def setup_logging():
     )
     
     logger = logging.getLogger(__name__)
-    logger.info("Application logging initialized")
+    logger.info("Application logging initialized.")
+
+
+def initialize_database():
+    logger = logging.getLogger(__name__)
+    logger.info("Initializing database.")
+    init_database()
+    logger.info("Database initialized successfully.")
+
+
+def create_repositories(connection):
+    logger = logging.getLogger(__name__)
+    logger.info("Creating repository instances.")
     
+    user_repository = UserRepository(connection)
+    event_repository = EventRepository(connection)
+    registration_repository = RegistrationRepository(connection)
+
+    logger.info("Repository instances created successfully.")
+
+    return(user_repository, event_repository, registration_repository)
+
+def create_services(user_repository, event_repository, registration_repository):
+    logger = logging.getLogger(__name__)
+    logger.info("Creating services instance,")
+
+    user_service = UserService(user_repository)
+    event_service = EventService(event_repository, registration_repository)
+
+    logger.info("Services instance successfully created.")
+    return(user_service, event_service)
+    
+
+def main():
