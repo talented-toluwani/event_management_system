71495f3cfe6dcd35fd8035052a3e431571c77597
Toluwani Edgal <edgaltoluwani@gmail.com>
2026-03-20 02:43:05 +0100

    New changes

--- event_management_system/main.py
+++ event_management_system/main.py
@@ -7,7 +7,7 @@ from user_repository import UserRepository
 from registration_repository import RegistrationRepository
 from event_service import EventService
 from user_service import UserService
-import menu_handler
+from menu_handler import MenuHandler
 
 
 def setup_logging():
@@ -51,3 +51,40 @@ def create_services(user_repository, event_repository, registration_repository):
     
 
 def main():
+    setup_logging()
+
+    logger = logging.getLogger(__name__)
+    logger.info("Starting Event Management System.")
+
+    try:
+        initialize_database()
+        connection = get_connection()
+        user_repository, event_repository, registration_repository = create_repositories(connection)
+        user_service, event_service = create_services(user_repository, event_repository, registration_repository)
+        menu = MenuHandler(user_service, event_service)
+
+        logger.info("Application initialized successfully.")
+        logger.info("Starting user interface...")
+        menu.run()
+
+    except KeyboardInterrupt:
+        logger.info("Application interrupted by user.")
+        print("\n\n Application terminated by user. Goodbye!")
+        sys.exit(0)
+
+    except Exception as e:
+        logger.error(f"Application error: {e}", exc_info=True)
+        print("An error occurred. Please check logs for details.")
+        sys.exit(1)
+
+    finally:
+
+        if 'connection' in locals() and connection:
+            connection.close()
+            logger.info("Database connection closed")
+    
+   
+        logger.info("Application shutdown complete")
+
+if __name__ == "__main__":
+    main()
