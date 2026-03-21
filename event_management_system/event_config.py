8d2a95218d1249acd32e10c1ebd0fc32442181ff
Toluwani Edgal <edgaltoluwani@gmail.com>
2026-03-21 18:28:53 +0100

    Made changes to the file path

--- event_management_system/event_config.py
+++ event_management_system/event_config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_PATH = BASE_DIR / "db" / "event_management.db"
+
+BASE_DIR = Path(__file__).resolve().parent
+DATABASE_PATH = BASE_DIR / "event_management.db"
 
 DATE_FORMAT = "%Y-%m -%d %H:%M"
 DISPLAY_DATE_FORMAT =  "%B %d, %Y at %I:%M %p"
